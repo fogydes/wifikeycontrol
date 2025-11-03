ba687a0b4fe8619f6459c2c70f65bc099b0266a6
fogydes <zeherop2@gmail.com>
2025-11-03 06:38:25 +0000

    update

--- android-app/app/src/main/java/com/wifikeycontrol/services/InputSimulatorService.kt
+++ android-app/app/src/main/java/com/wifikeycontrol/services/InputSimulatorService.kt
@@ -9,6 +9,7 @@ import android.content.IntentFilter
 import android.graphics.Path
 import android.graphics.PixelFormat
 import android.os.Build
+import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
