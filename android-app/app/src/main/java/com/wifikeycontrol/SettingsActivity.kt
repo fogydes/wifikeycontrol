8c6df74eeb3fca3de4225659841e2c387434ddcc
fogydes <zeherop2@gmail.com>
2025-11-03 06:27:44 +0000

    update

---
+++ android-app/app/src/main/java/com/wifikeycontrol/SettingsActivity.kt
@@ -0,0 +1,14 @@
+package com.wifikeycontrol
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        // Settings will be implemented in Phase 3
+        title = "Settings"
+    }
+}
